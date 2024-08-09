0a2795a57dcc720b96c9e1a53778841ec8753b17
Walter Kopacz <walterkopacz@Walters-MacBook-Air.local>
2024-08-08 20:40:50 -0400

    cleanup

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -176,12 +176,6 @@ void TheVerbAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
@@ -261,7 +255,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout TheVerbAudioProcessor::creat
 }
 
 //==============================================================================
-// This creates new instances of the plugin..
+
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new TheVerbAudioProcessor();
